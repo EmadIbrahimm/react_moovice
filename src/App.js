2a48d3fd9014528e8568ad37d5e3cc26cc8c0e69
Emad <emad.ibrahim533@gmail.com>
2020-07-30 17:55:55 +0200

    moovice1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Link,
+  Switch
+} from 'react-router-dom';
+import Discover from './components/Discover';
+import Popular from './components/Popular';
+import DiscoverBattle from './components/DiscoverBattle';
+import MyList from './components/MyList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return(
+      <Router>
+        <div>
+        <nav>
+          <ul>
+            <li><Link to ="/">discover</Link></li>
+            <li><Link to ="/battle">This week</Link></li>
+            <li><Link to ="/popular/">Popular</Link></li>
+            <li><Link to ="/my-List/">My list</Link></li>
+            <li><Link to ="/Popular-battle/">Popular-battle</Link></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Router path="/dicover/">
+            <Discover />
+          </Router>
+          <Router path="/Popular">
+            <Popular />
+
+          </Router>
+          <Router path="popular-battle">
+            <DiscoverBattle/>
+
+          </Router>
+          <Router path="this week">
+            <MyList/>
+
+          </Router>
+        </Switch>
+      </div>
+
+      </Router>
+      
+
+    );
+  }
 }
-
 export default App;
