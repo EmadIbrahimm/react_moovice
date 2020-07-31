2e7dcdaad588d33107265c284f8e4d6780ccc875
Emad <emad.ibrahim533@gmail.com>
2020-07-31 19:01:24 +0200

    exe vend

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Discover from './components/Discover';
 import Popular from './components/Popular';
 import DiscoverBattle from './components/DiscoverBattle';
 import MyList from './components/MyList';
+import PopularBattle from './components/PopularBattle';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class App extends React.Component {
@@ -17,29 +19,35 @@ class App extends React.Component {
         <div>
         <nav>
           <ul>
-            <li><Link to ="/">discover</Link></li>
-            <li><Link to ="/battle">This week</Link></li>
+            <li><Link to ="/">This week</Link></li>
+            <li><Link to ="/battle">This week battle</Link></li>
             <li><Link to ="/popular/">Popular</Link></li>
-            <li><Link to ="/my-List/">My list</Link></li>
             <li><Link to ="/Popular-battle/">Popular-battle</Link></li>
+            <li><Link to ="/my-List/">My list</Link></li>
           </ul>
         </nav>
         <Switch>
-          <Router path="/dicover/">
-            <Discover />
+        <Router path="/battle">
+            <DiscoverBattle />
+
           </Router>
           <Router path="/Popular">
             <Popular />
 
           </Router>
           <Router path="popular-battle">
-            <DiscoverBattle/>
+            <PopularBattle />
 
           </Router>
-          <Router path="this week">
+          <Router path="this-week">
             <MyList/>
 
           </Router>
+
+          <Router path="/">
+            <Discover />
+          </Router>
+
         </Switch>
       </div>
 
--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,9 +1,28 @@
 import React from 'react';
 class Popular extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            movies: [],
+            isLoaded: false,
+        }
+    }
+    componentDidMount() {
+        fetch('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=<API_KEY>')
+        .then(res => res.json())
+        .then(json => {
+            this.setState({
+                isLoaded:true,
+                movies: json,
+
+            });
+            
+        })
+    }
     render() {
         return(
             <div>
-                This week
+                Popular
             </div>
 
         )
